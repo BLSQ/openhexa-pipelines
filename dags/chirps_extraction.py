0f2c820cd490887049ad8065072c2ec4cb1ddf63
hastakecorp <github@hastake.com>
2021-10-05 09:00:48 +0200

    Add a chirps extraction DAG

---
+++ dags/chirps_extraction.py
@@ -0,0 +1,11 @@
+from datetime import datetime
+
+from airflow import DAG
+from airflow.operators.bash import BashOperator
+
+
+with DAG('chirps-extraction', description='CHIRPS Extraction', schedule_interval='*/10 * * * *', start_date=datetime(2021, 10, 1), catchup=False) as dag:
+    download = BashOperator(task_id='chirps-download', bash_command="echo docker run download")
+    extract = BashOperator(task_id='chirps-extract', bash_command="echo docker run extract")
+
+    download >> extract
