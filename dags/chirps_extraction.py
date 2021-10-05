a6411895122c9c19ee664cb99d0794c656d07bfd
hastakecorp <github@hastake.com>
2021-10-05 19:39:56 +0200

    [enh] use a gke pod started instead of a docker operator

--- dags/chirps_extraction.py
+++ dags/chirps_extraction.py
@@ -2,26 +2,25 @@ from datetime import datetime
 
 from airflow import DAG
 from airflow.models import Variable
-from airflow.providers.docker.operators.docker import DockerOperator
+from airflow.providers.google.cloud.operators.kubernetes_engine import GKEStartPodOperator
+from airflow.operators.bash import BashOperator
 
 
-test_var = Variable.get("SECRET_TEST_VAR")
+test_var = "A" # Variable.get("SECRET_TEST_VAR")
 
 
 with DAG('chirps-extraction', description='CHIRPS Extraction', schedule_interval='30 * * * *', start_date=datetime(2021, 10, 1), catchup=False) as dag:
-    download = DockerOperator(
+    download = GKEStartPodOperator(
+        location="europe-west1-b",
+        cluster_name="hexa-aldebaran",
+        project_id="blsq-dip-test",
+        namespace="hexa-airflow",
+        name="chirps-download-pod",
         image="blsq/chirps-extraction",
-        docker_url='tcp://localhost:2375',
         command="download --start 2021 --end 2021 --output-dir /tmp/chirps/",
-        task_id='chirps-download',
-        environment={"TEST_VAR": test_var},
+        task_id="chirps-download",
+        env_vars={"TEST_VAR": test_var},
     )
-    extract = DockerOperator(
-        image="blsq/chirps-extraction",
-        docker_url='tcp://localhost:2375',
-        command="extract --start 2021 --end 2021",
-        task_id='chirps-extract',
-        environment={"TEST_VAR": test_var},
-    )
+    extract = BashOperator(task_id='chirps-extract', bash_command="echo docker run extract")
 
     download >> extract
