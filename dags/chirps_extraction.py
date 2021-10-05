ca779891abdccd80045a92eab10911215b0f555b
hastakecorp <github@corp.hastake.com>
2021-10-05 21:23:40 +0200

    [enh] try with KubernetesPodOperator instead of gke/no gcloud?

--- dags/chirps_extraction.py
+++ dags/chirps_extraction.py
@@ -2,7 +2,7 @@ from datetime import datetime
 
 from airflow import DAG
 from airflow.models import Variable
-from airflow.providers.google.cloud.operators.kubernetes_engine import GKEStartPodOperator
+from airflow.providers.cncf.kubernetes.operators.kubernetes_pod import KubernetesPodOperator
 from airflow.operators.bash import BashOperator
 
 
@@ -10,16 +10,15 @@ test_var = "A" # Variable.get("SECRET_TEST_VAR")
 
 
 with DAG('chirps-extraction', description='CHIRPS Extraction', schedule_interval='30 * * * *', start_date=datetime(2021, 10, 1), catchup=False) as dag:
-    download = GKEStartPodOperator(
-        location="europe-west1-b",
-        cluster_name="hexa-aldebaran",
-        project_id="blsq-dip-test",
-        namespace="hexa-airflow",
-        name="chirps-download-pod",
+    download = KubernetesPodOperator(
+        namespace='hexa-airflow',
         image="blsq/chirps-extraction",
         arguments="download --start 2021 --end 2021 --output-dir /tmp/chirps/".split(" "),
+        name="chirps-download-pod",
         task_id="chirps-download",
         env_vars={"TEST_VAR": test_var},
+        in_cluster=True,
+        get_logs=True,
     )
     extract = BashOperator(task_id='chirps-extract', bash_command="echo docker run extract")
 
