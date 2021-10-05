a7c2dc7f7d88df126a8920283a77ff9b11ad6ffd
hastakecorp <github@hastake.com>
2021-10-05 16:57:02 +0200

    [enh] bypass anti secret protection

--- dags/chirps_extraction.py
+++ dags/chirps_extraction.py
@@ -9,7 +9,7 @@ test_var = Variable.get("SECRET_TEST_VAR")
 
 
 with DAG('chirps-extraction', description='CHIRPS Extraction', schedule_interval='30 * * * *', start_date=datetime(2021, 10, 1), catchup=False) as dag:
-    download = BashOperator(task_id='chirps-download', bash_command="echo docker run download $TEST_VAR", env={"TEST_VAR": test_var})
+    download = BashOperator(task_id='chirps-download', bash_command="echo docker run download $TEST_VAR", env={"TEST_VAR": " ".join(str(test_var))})
     extract = BashOperator(task_id='chirps-extract', bash_command="echo docker run extract")
 
     download >> extract
