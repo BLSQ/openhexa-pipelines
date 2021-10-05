9aac2efdaa8dc6fecba816f847c55cdd95ff63a9
hastakecorp <github@hastake.com>
2021-10-05 16:13:51 +0200

    [enh] decrease chirps extraction freq and test dag sync

--- dags/chirps_extraction.py
+++ dags/chirps_extraction.py
@@ -4,7 +4,7 @@ from airflow import DAG
 from airflow.operators.bash import BashOperator
 
 
-with DAG('chirps-extraction', description='CHIRPS Extraction', schedule_interval='*/10 * * * *', start_date=datetime(2021, 10, 1), catchup=False) as dag:
+with DAG('chirps-extraction', description='CHIRPS Extraction', schedule_interval='30 * * * *', start_date=datetime(2021, 10, 1), catchup=False) as dag:
     download = BashOperator(task_id='chirps-download', bash_command="echo docker run download")
     extract = BashOperator(task_id='chirps-extract', bash_command="echo docker run extract")
 
