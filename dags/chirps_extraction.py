1d408dd185ab3e656dd2ab39322f4db9097728fa
hastakecorp <github@corp.hastake.com>
2021-10-06 11:27:06 +0200

    [enh] update DAG

--- dags/chirps_extraction.py
+++ dags/chirps_extraction.py
@@ -9,17 +9,26 @@ from airflow.operators.bash import BashOperator
 test_var = "A" # Variable.get("SECRET_TEST_VAR")
 
 
-with DAG('chirps-extraction', description='CHIRPS Extraction', schedule_interval='30 * * * *', start_date=datetime(2021, 10, 1), catchup=False) as dag:
+with DAG('chirps-extraction', description='CHIRPS Extraction', schedule_interval='0 1 27 * *', start_date=datetime(2021, 10, 1), catchup=False) as dag:
     download = KubernetesPodOperator(
         namespace='hexa-airflow',
         image="blsq/chirps-extraction",
-        arguments="chirps download --start 2021 --end 2021 --output-dir /tmp/chirps/".split(" "),
+        arguments="chirps download --start 2020 --end 2020 --output-dir s3://hexa-demo-pipeline-chirps/download/".split(" "),
         name="chirps-download-pod",
         task_id="chirps-download",
         env_vars={"TEST_VAR": test_var},
         in_cluster=True,
         get_logs=True,
     )
-    extract = BashOperator(task_id='chirps-extract', bash_command="echo docker run extract")
+    extract = KubernetesPodOperator(
+        namespace='hexa-airflow',
+        image="blsq/chirps-extraction",
+        arguments="chirps extract --start 2020 --end 2020 --input-dir s://hexa-demo-pipeline-chirps/download/ --output-file s3://hexa-demo-pipeline-chirps/extract.csv  --contours s3://hexa-demo-pipeline-chirps/world/BFA.geo.json ".split(" "),
+        name="chirps-extract-pod",
+        task_id="chirps-extract",
+        env_vars={"TEST_VAR": test_var},
+        in_cluster=True,
+        get_logs=True,
+    )
 
     download >> extract
