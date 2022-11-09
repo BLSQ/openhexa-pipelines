e590f773adec0c3843288cdd2185aeba3179f0c4
Yann Forget <yannforget@mailbox.org>
2022-11-09 19:23:04 +0100

    Fix incorrect parameter check

--- dhis2-extraction/dhis2extract.py
+++ dhis2-extraction/dhis2extract.py
@@ -383,9 +383,9 @@ def _check_parameters(**kwargs):
             raise ValueError(msg)
 
     data_element = (
-        kwargs["data_element"] and kwargs["data_elements_group"] and kwargs["datasets"]
+        kwargs["data_element"] and kwargs["data_element_group"] and kwargs["datasets"]
     )
-    indicator = kwargs["indicator"] and kwargs["indicators"]
+    indicator = kwargs["indicator"] and kwargs["indicator_group"]
     if not data_element and not indicator:
         msg = "No data element or indicator provided"
         dag.log_message("ERROR", msg)
